974ebfa80ee1838ad42a14bedbeae8b0ea5de782
felfert <felfert@fd6afe1d-0a44-482b-9153-41019ba58a45>
2011-12-13 11:02:42 +0000

    - Code cleanup
    git-svn-id: http://svn.code.sf.net/p/ehs/code/trunk@73 fd6afe1d-0a44-482b-9153-41019ba58a45

--- securesocket.cpp
+++ securesocket.cpp
@@ -179,8 +179,7 @@ retry:
     SSL_set_fd(ssl, fd);
     int ret = SSL_accept(ssl);
     if (1 != ret) {
-        int err = s_pSslError->GetError(sError);
-//# int err = s_pSslError->GetErrorString(sError, SSL_get_error(ssl, ret));
+        s_pSslError->GetError(sError);
         sError.append(" from ");
         sError.append(GetPeer());
         SSL_free(ssl);
